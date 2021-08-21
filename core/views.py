a2fa0a292519de4fdbe4d36b45bc5a55dcd39e56
mjbiener <michaelbiener@yahoo.com>
2021-08-21 12:39:18 -0400

    added index, feed, profile basic views/html linking

--- core/views.py
+++ core/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import User, Profile, Snippet
+
 
 # Create your views here.
 def index(request):
-    return render(
-        request,
-        'core/index.html'
-    )
+    users = User.objects.all()
+    if request.user.is_authenticated:
+        return redirect(to='feed')
+    return render(request, 'core/index.html', {'users': users})
+
+    
+def user_profile(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    profile = get_object_or_404(Profile, user=user)
+    
+    return render(request, 'core/user_profile.html', {'user': user, 'profile': profile})
+
+def feed(request):
+    users = User.objects.all()
+    
+
+    return render(request, 'core/feed.html', {'users':users})
