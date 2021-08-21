57628b9943b324d623da69ef55b8acefba6042df
andreavaughan <amvaughan16@gmail.com>
2021-08-21 11:59:39 -0400

    created basic models, superuser

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import DateTimeField
 
 # Create your models here.
 
@@ -10,3 +11,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', primary_key=True)
+    profile_image = models.ImageField(upload_to='profile', default="media/default_image.jpg")
+    bio = models.TextField(max_length=400, null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user}"
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippet')
+    code = models.TextField()
+    language = models.CharField(max_length=30)
+    created_at = DateTimeField(auto_now_add=True)
+    copied = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.title}'
