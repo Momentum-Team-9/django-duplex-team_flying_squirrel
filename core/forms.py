1b9aabd65368ba874089f8f5e52dc4d7a1658f24
mjbiener <michaelbiener@yahoo.com>
2021-08-22 18:03:48 -0400

    edit profile form loads saves new bio - but not new pic

--- core/forms.py
+++ core/forms.py
@@ -13,17 +13,6 @@ class ProfileForm(forms.ModelForm):
         fields = ['user', 'profile_image', 'bio']
 
 
-# class EditProfileForm(forms.ModelForm):
-# 
-    # def __init__(self, *args, **kwargs):
-    #     super(ProfileForm, self).__init__(*args, **kwargs)
-    #     self.fields['user'].disabled = True
-    #     self.fields['user'].widget = forms.HiddenInput()
-    # class Meta:
-    #     model = Profile
-    #     fields = ['profile_image', 'bio', 'user']
-
-
 class SnippetForm(forms.ModelForm):
 
     class Meta:
